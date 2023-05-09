78f7a1800931fe6512fc5f4fe7361e413d9adb05
MacBook de Tuyen <tuyen@MacBook-Pro-de-MacBook.local>
2023-05-09 14:49:56 +0200

    feat: added login with hashed password

--- quiz-api/app.py
+++ quiz-api/app.py
@@ -1,5 +1,6 @@
-from flask import Flask
+from flask import Flask, request
 from flask_cors import CORS
+import hashlib
 
 app = Flask(__name__)
 CORS(app)
@@ -12,6 +13,17 @@ def hello_world():
 @app.route('/quiz-info', methods=['GET'])
 def GetQuizInfo():
 	return {"size": 0, "scores": []}, 200
+
+@app.route('/login', methods=['POST'])
+def PostPassword():
+	payload = request.get_json()
+	tried_password = payload['password'].encode('UTF-8')
+	hashed = hashlib.md5(tried_password).digest()
+
+	if(hashed==b'\xd8\x17\x06PG\x92\x93\xc1.\x02\x01\xe5\xfd\xf4_@'):
+		return {"token": "sampleToken"}, 200
+	else :
+		return "Unauthorized", 401
 
 if __name__ == "__main__":
     app.run()
