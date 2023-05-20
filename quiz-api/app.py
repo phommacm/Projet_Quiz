b1bb917ac92a306fa5c80a294a6fefb30c2d4b25
phommacm <matthieu.phommachanh@edu.esiee.fr>
2023-05-20 17:34:38 +0200

    chore: adding comments for more clarity

--- quiz-api/app.py
+++ quiz-api/app.py
@@ -9,35 +9,45 @@ from jwt_utils import build_token
 app = Flask(__name__)
 CORS(app)
 
+# Endpoint pour tester l'application
 @app.route('/')
 def hello_world():
 	x = 'world'
 	return f"Hello, {x}"
 
+# Endpoint pour obtenir des informations sur le quiz
 @app.route('/quiz-info', methods=['GET'])
 def GetQuizInfo():
 	return {"size": 0, "scores": []}, 200
 
+# Endpoint pour gérer la connexion
 @app.route('/login', methods=['POST'])
 def PostLogin():
 	payload = request.get_json()
 	tried_password = payload['password'].encode('UTF-8')
 	hashed = hashlib.md5(tried_password).digest()
 
+	# Renvoie du jeton dans le cas où le mot de passe est correct
 	if (hashed == b'\xd8\x17\x06PG\x92\x93\xc1.\x02\x01\xe5\xfd\xf4_@'):
 		return {"token": build_token()}, 200
 	else:
 		return "Unauthorized", 401
 
+# Endpoint pour ajouter une question
 @app.route('/questions', methods=['POST'])
 def PostAddQuestion():
+    # Récupération des données de la question envoyées dans le corps de la requête JSON
     question_data = request.get_json()
     
+	# Insertion de la question dans la base de données
     conn = sqlite3.connect('./quiz-questions.db')
     cursor = conn.cursor()
     cursor.execute("INSERT INTO quiz_questions (position, title, text, image) VALUES (?, ?, ?, ?)",
                    (question_data['position'], question_data['title'], question_data['text'], question_data['image']))
+    
+	# Récupération de l'identifiant de la question que l'on vient d'insérée
     question_id = cursor.lastrowid
+    
     conn.commit()
     conn.close()
 
