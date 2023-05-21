20dbb303196ee9dc4ac819b3af8a957529508adf
phommacm <matthieu.phommachanh@edu.esiee.fr>
2023-05-21 14:56:11 +0200

    feat: adding question class and modifying add_question consequently

--- quiz-api/question_utils.py
+++ quiz-api/question_utils.py
@@ -3,6 +3,13 @@ from flask import request
 from jwt_utils import decode_token
 from jwt_utils import JwtError
 
+class Question:
+    def __init__(self, position: int, title: str, text: str, image: str):
+        self.position = position
+        self.title = title
+        self.text = text
+        self.image = image
+
 def add_question():
     # Récupération des données de la question envoyées dans le corps de la requête JSON
     question_data = request.get_json()
@@ -25,11 +32,14 @@ def add_question():
     else:
         return "Unauthorized", 401
 
+    # Création d'une instance de la classe Question avec les données de la question
+    question = Question(question_data['position'], question_data['title'], question_data['text'], question_data['image'])
+
     # Insertion de la question dans la base de données
     conn = sqlite3.connect('./quiz-questions.db')
     cursor = conn.cursor()
     cursor.execute("INSERT INTO quiz_questions (position, title, text, image) VALUES (?, ?, ?, ?)",
-                   (question_data['position'], question_data['title'], question_data['text'], question_data['image']))
+                   (question.position, question.title, question.text, question.image))
     
     # Récupération de l'identifiant de la question que l'on vient d'insérer
     question_id = cursor.lastrowid
